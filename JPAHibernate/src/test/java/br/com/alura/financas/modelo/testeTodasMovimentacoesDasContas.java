2dcc34b282102bba8c103473460f9366daea2e9e
juniorsolo <junior.solo@hotmail.com>
2017-07-13 00:00:51 -0300

    Obtendo todas as movimentações das contas( resolvendo o problema do n+1 )

---
+++ JPAHibernate/src/test/java/br/com/alura/financas/modelo/testeTodasMovimentacoesDasContas.java
@@ -0,0 +1,36 @@
+package br.com.alura.financas.modelo;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import br.com.alura.financas.util.JPAUtil;
+
+public class testeTodasMovimentacoesDasContas {
+
+	public static void main(String[] args) {
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+		String jpql = "select c from Conta c join fetch c.movimentacoes";
+		
+		Query query = em.createQuery(jpql);
+		
+		List<Conta> todasContas = query.getResultList();
+		
+		System.out.println("QTD: " + todasContas.size());
+		
+		for (Conta conta : todasContas) {
+			System.out.println("titular: " + conta.getTitular());
+			System.out.println("Movimentacoes: ");
+
+			for (Movimentacao mov : conta.getMovimentacoes()) {
+				System.out.println("Desc: "+ mov.getDescricao());
+			}
+		}
+		
+		em.getTransaction().commit();		
+		em.close();
+
+	}
+
+}
